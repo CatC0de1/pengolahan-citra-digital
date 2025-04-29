080aee05367f2114ac82cb2672e0483826b90c0e
Iyan Zuli Armanda <catcode0101@gmail.com>
2025-04-29 14:39:12 +0700

    task7: cpp prototyping

---
+++ tugas7/app.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include "components/binary_image.hpp"
+#include <fstream>
+
+using namespace std;
+using namespace cv;
+
+// Fungsi untuk menyimpan matrix ke file txt
+void saveFilteredMatrix(const Mat& matrix, const string& filename) {
+  ofstream txtFile(filename);
+  if (txtFile.is_open()) {
+    for (int i = 0; i < matrix.rows; ++i) {
+      for (int j = 0; j < matrix.cols; ++j) {
+        txtFile << static_cast<int>(matrix.at<uchar>(i, j));
+        if (j != matrix.cols - 1) txtFile << "\t";
+      }
+      txtFile << "\n";
+    }
+    txtFile.close();
+    cout << "Matrix hasil filter berhasil disimpan ke '" << filename << "'\n";
+  } else {
+    cerr << "Gagal membuka file untuk menulis matrix.\n";
+  }
+}
+
+int main() {
+  // Konversi citra biner ke Mat
+  Mat inputMat(ROWS, COLS, CV_8UC1);
+  for (int i = 0; i < ROWS; ++i)
+    for (int j = 0; j < COLS; ++j)
+      inputMat.at<uchar>(i, j) = binaryImage[i][j] * 255; // konversi ke 0 dan 255
+
+  // Structuring element 3x3 
+  // [{1, 1, 1},
+  // {1, 1, 1},
+  // {1, 1, 1}]
+  Mat structElem = getStructuringElement(MORPH_RECT, Size(3, 3));
+
+  // Operasi morfologi
+  Mat dilated, eroded, opened, closed;
+  dilate(inputMat, dilated, structElem);
+  erode(inputMat, eroded, structElem);
+  morphologyEx(inputMat, opened, MORPH_OPEN, structElem);
+  morphologyEx(inputMat, closed, MORPH_CLOSE, structElem);
+
+  // Tampilkan hasil
+  imshow("Original", inputMat);
+  imshow("Dilated", dilated);
+  imshow("Eroded", eroded);
+  imshow("Opened", opened);
+  imshow("Closed", closed);
+
+  // Simpan hasil dalam format jpg
+  imwrite("./output/jpg/Original.jpg", inputMat);
+  imwrite("./output/jpg/Dilated.jpg", dilated);
+  imwrite("./output/jpg/Eroded.jpg", eroded);
+  imwrite("./output/jpg/Opened.jpg", opened);
+  imwrite("./output/jpg/Closed.jpg", closed);
+
+  // Simpan hasil dalam format txt
+  saveFilteredMatrix(inputMat, "./output/txt/Original.txt");
+  saveFilteredMatrix(dilated, "./output/txt/Dilated.txt");
+  saveFilteredMatrix(eroded, "./output/txt/Eroded.txt");
+  saveFilteredMatrix(opened, "./output/txt/Opened.txt");
+  saveFilteredMatrix(closed, "./output/txt/Closed.txt");
+
+  cout << "\nTekan ESC pada citra untuk menutup dan kembali ke menu.\n";
+  cout << "Seret jendela citra untuk melihat citra yang lain.\n";
+
+  while (true) {
+    int key = waitKey(0);
+    if (key == 27) { // ESC key
+      cout << "Keluar dari program.\n";
+      destroyAllWindows();
+      return 0;
+    }
+  }
+}
+
+
+/*
+g++ app.cpp -o Demo7 `pkg-config --cflags --libs opencv4`
+./Demo7
+*/
